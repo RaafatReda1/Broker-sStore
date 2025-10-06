85425825221d0516ec1fbe3d679a103509ec1128
Raafat <rafat2782005@gmail.com>
2025-10-06 05:04:12 +0300

    Additional updates

--- src/Components/Manage/Moderator/Moderator.jsx
+++ src/Components/Manage/Moderator/Moderator.jsx
@@ -3,17 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import ManagingDashboard from "../ManagingDashboard/ManagingDashboard";
 import ManageBrokers from "../ManageBrokers/ManageBrokers";
 import ManageOrders from "../ManageOrders/ManageOrders";
-
+import "./Moderator.css";
 const Moderator = () => {
   console.log("👮 Moderator Component Rendered - User has MODERATOR access");
 
   return (
-    <>
+    <div className="moderator">
       <Routes>
         <Route path="/manageBrokers" element={<ManageBrokers />} />
         <Route path="/manageOrders" element={<ManageOrders />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
