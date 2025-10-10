87cbe2c43e17f11c51b65d08afd620542800f299
Raafat <rafat2782005@gmail.com>
2025-10-11 02:28:27 +0300

    adding aboutus route in the moderator comp

--- src/Components/Manage/Moderator/Moderator.jsx
+++ src/Components/Manage/Moderator/Moderator.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import ManagingDashboard from "../ManagingDashboard/ManagingDashboard";
 import ManageBrokers from "../ManageBrokers/ManageBrokers";
 import ManageOrders from "../ManageOrders/ManageOrders";
 import "./Moderator.css";
 import ManageNotifications from "../ManageNotifications/ManageNotifications";
+import AboutUs from "../../AboutUs/AboutUs";
 const Moderator = () => {
   console.log("👮 Moderator Component Rendered - User has MODERATOR access");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/aboutus");
+  }, []);
 
   return (
     <div className="moderator">
@@ -14,6 +20,7 @@ const Moderator = () => {
         <Route path="/manageBrokers" element={<ManageBrokers />} />
         <Route path="/manageOrders" element={<ManageOrders />} />
         <Route path="/manageNotifications" element={<ManageNotifications />} />
+        <Route path="/aboutus" element={<AboutUs />} />
       </Routes>
     </div>
   );
